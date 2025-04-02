497735aac5c68e84a0f65c4e70a5614c7f00027d
LucasK <lucas.kuntz@afpa-dev-pompey.fr>
2025-04-02 09:03:08 +0200

    @Data pour CustomProperties

--- src/main/java/config/CustomProperties.java
+++ src/main/java/config/CustomProperties.java
@@ -2,18 +2,10 @@ package config;
 
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
 
+@Data
 @ConfigurationProperties(prefix= "appweb.apiurl")
 public class CustomProperties {
 
     private String apiUrl;
-
-    public String getApiUrl() {
-        return apiUrl;
-    }
-
-    public void setApiUrl(String apiUrl) {
-        this.apiUrl = apiUrl;
-    }
 }
